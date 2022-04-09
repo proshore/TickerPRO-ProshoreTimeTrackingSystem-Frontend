f6149ca86902636c29da0afa44508ca0f64998f0
Asim Shrestha <asimshrestha608@gmail.com>
2022-04-09 08:19:02 +0545

    create: UI of the login page

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,33 @@
+<script setup>
+defineProps({
+  type: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  label: {
+    type: String,
+    required: true,
+  },
+  modelValue: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <label :for="name" class="form-label">{{ label }} </label>
+  <input
+    :type="type"
+    class="form-control"
+    :id="name"
+    :name="name"
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+    required
+  />
+</template>
--- src/modules/accounts/views/LoginView.vue
+++ src/modules/accounts/views/LoginView.vue
@@ -1,3 +1,40 @@
+<script setup>
+import { ref } from "vue";
+
+import H1Text from "@/components/H1Text.vue";
+import PText from "@/components/PText.vue";
+import BaseInput from "@/components/BaseInput.vue";
+
+const form = ref({
+  email: "",
+  password: "",
+});
+
+function handleLogin() {
+  console.log(form.value);
+}
+</script>
+
 <template>
-  <h1>Log in to Ticker.</h1>
+  <H1Text text="Log in to Ticker." />
+  <PText text="Welcome back!" />
+
+  <form @submit.prevent="handleLogin">
+    <div class="mb-3">
+      <BaseInput
+        type="email"
+        name="email"
+        label="Email address"
+        v-model="form.email"
+      />
+      <BaseInput
+        type="password"
+        name="password"
+        label="Password"
+        v-model="form.password"
+      />
+    </div>
+
+    <button type="submit" class="btn btn-secondary">Log in</button>
+  </form>
 </template>
