9fe954f24298d8c6da6bf52021b7bccd3685a0b0
Asim Shrestha <asimshrestha608@gmail.com>
2022-05-09 09:37:50 +0545

    style mail sent success page

--- src/modules/accounts/views/ViewPasswordResetSent.vue
+++ src/modules/accounts/views/ViewPasswordResetSent.vue
@@ -1,10 +1,26 @@
 <script setup>
 import BaseFormHeading from "@/components/BaseFormHeading.vue";
+
+import logo from "@/assets/images/logo.svg";
+import mailSentSuccess from "@/assets/images/mail-sent-success.svg";
 </script>
 
 <template>
-  <BaseFormHeading
-    title="Password reset email send"
-    shortDesc="We've emailed you instructions for setting your password."
-  />
+  <img :src="logo" alt="Ticker logo" class="logo" />
+
+  <div class="d-grid col-md-8 col-lg-5 mx-auto">
+    <BaseFormHeading
+      title="Check your mail"
+      shortDesc="Check your mail inbox for instructions from us on how to reset your password."
+    />
+
+    <img :src="mailSentSuccess" alt="Mail sent success logo" />
+
+    <p class="mt-4">
+      Never mind!
+      <RouterLink :to="{ name: 'login' }" class="text-secondary fw-normal"
+        ><u> Take me back to login</u></RouterLink
+      >
+    </p>
+  </div>
 </template>
