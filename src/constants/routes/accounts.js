70140cfa3a6af07d164c96a7e197d2c1117fd52d
Asim Shrestha <asimshrestha608@gmail.com>
2022-05-11 19:15:35 +0545

    refactor api routes name file into routes directory inside constants directory

--- src/constants/routes/accounts.js
+++ src/constants/routes/accounts.js
@@ -1,10 +1,10 @@
-const Signup_URL = "/api/user/register";
+const SIGNUP_URL = "/api/user/register";
 const LOGIN_URL = "/api/user/login";
 const FORGOT_PASSWORD_URL = "/api/user/forgot-password";
 const PASSWORD_RESET_URL = "/api/user/reset-password";
 
 export default {
-  Signup_URL,
+  SIGNUP_URL,
   LOGIN_URL,
   FORGOT_PASSWORD_URL,
   PASSWORD_RESET_URL,
