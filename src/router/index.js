9ee8978b6751ad340f27f38b9c0a9af6223c205f
Asim Shrestha <asimshrestha608@gmail.com>
2022-05-11 20:59:14 +0545

    refactor: use @ alias

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import accountsRoutes from "../modules/accounts/router";
-import dashboardRoutes from "../modules/dashboard/router";
-import teamsRoutes from "../modules/teams/router";
+import accountsRoutes from "@/modules/accounts/router";
+import dashboardRoutes from "@/modules/dashboard/router";
+import teamsRoutes from "@/modules/teams/router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
