f7134113cb9a514adc6f8385110d99f03e28be25
Binita-subedi <subedibinita94@gmail.com>
2022-05-14 13:42:17 +0545

    Configuration of TickerPRO logo with env or config file

--- src/config/app.js
+++ src/config/app.js
@@ -1,3 +1,5 @@
 const BACKEND_URI = import.meta.env.VITE_BACKEND_URI;
 
-export default { BACKEND_URI };
+const LOGO_PATH = "../src/assets/images/logo.svg";
+
+export default { BACKEND_URI, LOGO_PATH };
