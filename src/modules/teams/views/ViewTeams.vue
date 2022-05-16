4de27d2f75029c06a1ce25cc822d0bbc8d1193bb
Asim Shrestha <asimshrestha608@gmail.com>
2022-05-16 10:04:59 +0545

    create simple sidebar

--- src/modules/teams/views/ViewTeams.vue
+++ src/modules/teams/views/ViewTeams.vue
@@ -1,8 +1,10 @@
 <script setup>
 import TheLogoutLink from "@/components/TheLogoutLink.vue";
+import TheSidebar from "@/components/TheSidebar.vue";
 </script>
 
 <template>
+  <TheSidebar />
   <h1>Teams page</h1>
   <TheLogoutLink />
 </template>
