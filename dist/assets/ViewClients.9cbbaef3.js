93d32470c3d18fc5d0be35a0abc24b24cd5aaece
Asim Shrestha <asimshrestha608@gmail.com>
2022-06-08 15:43:15 +0545

    build: change backend api url

--- dist/assets/ViewClients.9cbbaef3.js
+++
@@ -1,1 +0,0 @@
-import{E as t,o as s,c as o,b as e,a as r,F as n}from"./index.e8fa8bee.js";import{T as c,_ as l}from"./BaseHeading.181f477a.js";import"./logo.bccff2c0.js";import"./plugin-vue_export-helper.21dcd24c.js";const d={setup(i){const a=t();return a.userStatus=!1,(m,p)=>(s(),o(n,null,[e(c),r("main",null,[e(l,{title:"Clients"})])],64))}};export{d as default};
