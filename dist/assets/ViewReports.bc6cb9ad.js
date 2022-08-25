f7edeca81388e71576b13c1c617bbbd89ed4e755
shrestharikesh <rikeshshrestha2052@gmail.com>
2022-08-25 13:43:56 +0545

    asset compiled for prod

---
+++ dist/assets/ViewReports.bc6cb9ad.js
@@ -0,0 +1,1 @@
+import{H as t,o,c as r,b as e,a as s,F as n}from"./index.2f13ce3f.js";import{T as c,_ as l}from"./BaseHeading.6ba9692b.js";import"./logo.f5257f33.js";import"./plugin-vue_export-helper.21dcd24c.js";const d={setup(i){const a=t();return a.userStatus=!1,(m,p)=>(o(),r(n,null,[e(c),s("main",null,[e(l,{title:"Reports"})])],64))}};export{d as default};
