a474216f4084c7a97f7d904671cc83827a15dbcf
Surya Shrestha <shresthasurya92@gmail.com>
2022-09-27 15:02:50 +0545

    PTTB-121-final changes removed console log too as well as 6 digit required script for login

--- src/utils/validatePassword.js
+++ src/utils/validatePassword.js
@@ -1,6 +1,5 @@
 export default function (value) {
   const response = { isValid: true, errorMessage: "" };
-  console.log(value)
   // if value is empty
   if (!value) {
     response.isValid = false;
