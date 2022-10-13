c3c1fb1866981d311f8dffebd2cffe229befb1ac
Nischal Khadka <justchalchal@gmail.com>
2022-10-13 10:50:53 +0545

    PTTB-157-frontend-in-add-new-member-modal-in-teams-section-the-modal-does-not-close-automatically

--- src/modules/teams/components/InviteMember.vue
+++ src/modules/teams/components/InviteMember.vue
@@ -21,9 +21,9 @@ const errors = ref([]);
 const $toast = useToast();
 
 const props = defineProps({
-  handleInvitedMembers: Function
-})
-const {handleInvitedMembers} = props
+  handleInvitedMembers: Function,
+});
+const { handleInvitedMembers } = props;
 
 async function allRoles() {
   try {
@@ -83,15 +83,12 @@ async function handleInviteMember() {
         successInvite.value = true;
         handleInvitedMembers();
         $toast.success("Invite is sent successfully!!!");
+        clearModal();
+        modalStatus.value = false;
         // reset successInvite after 3 seconds
         setTimeout(() => {
           successInvite.value = false;
         }, 1500);
-
-        // empty form fields
-        name.value = "";
-        email.value = "";
-        role.value = "";
       }
     } catch (error) {
       $toast.error("Something went wrong, please try again later.");
@@ -99,49 +96,60 @@ async function handleInviteMember() {
   }
 }
 
-function closeModel() { 
-    name.value = "";
-    email.value = "";
-    role.value = "";
+const modalStatus = ref(false);
+
+function showModal(){
+  modalStatus.value = true;
 }
+
+function closeModal(){
+  modalStatus.value = false;
+  clearModal();
+}
+
+function clearModal() {
+  name.value = "";
+  email.value = "";
+  role.value = "";
+}
+
 </script>
 <style>
 th,
 td {
   cursor: pointer;
 }
+.popup {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 99;
+  background-color: rgba(0, 0, 0, 0.2);
+  align-items: center;
+}
 </style>
 
 <template>
   <!-- Button trigger modal -->
-  <button
-    type="button"
-    class="btn btn-sm btn-primary text-white"
-    data-bs-toggle="modal"
-    data-bs-target="#exampleModal"
-  >
+  <button type="button" class="btn btn-sm btn-primary text-white" @click="showModal">
     Add New Member
   </button>
 
   <!-- Modal -->
-  <div
-    class="modal fade"
-    id="exampleModal"
-    tabindex="-1"
-    aria-labelledby="exampleModalLabel"
-    aria-hidden="true"
-  >
+  <div v-if="modalStatus" class="popup">
     <div class="modal-dialog">
-      <div class="modal-content" >
+      <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title" id="exampleModalLabel">Add New Member</h5>
           <button
-            @click="closeModel"
+            @click="closeModal"
             type="button"
             class="btn-close"
             data-bs-dismiss="modal"
             aria-label="Close"
-            data-cy = "clearFieldWhenClose"
+            data-cy="clearFieldWhenClose"
           ></button>
         </div>
         <div class="modal-body">
