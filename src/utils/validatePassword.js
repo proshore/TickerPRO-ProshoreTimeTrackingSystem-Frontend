d8a0f936cb257a5ee038deacf369a096e82a41be
Surya Shrestha <shresthasurya92@gmail.com>
2022-10-16 17:31:57 +0545

    PTTB-159 new js file created for signUp password validation where user must have one special character atleast 6 digit and one numeric value

--- src/utils/validatePassword.js
+++ src/utils/validatePassword.js
@@ -1,10 +1,17 @@
 export default function (value) {
   const response = { isValid: true, errorMessage: "" };
+  // let paswd=  /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,15}$/; // atleast 6 digit and one special character and one numeric digit 
   // if value is empty
   if (!value) {
     response.isValid = false;
     response.errorMessage = "Password field is required";
     return response;
-  } 
+  }
+//    else if (!paswd.test(value)){
+//     // if not matching passsword 
+//     response.isValid = false;
+//     response.errorMessage = "Please use atleast one numeric digit and one special character";
+//     return response;
+// }
   return response;
 }
